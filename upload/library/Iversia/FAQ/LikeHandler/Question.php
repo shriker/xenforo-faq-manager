0aa697c4b34107699713ae4bed4640396ecad279
Jodie Struthers <jodie@iversia.com>
2013-02-12 12:28:09 -0700

    Fixed function call.

--- upload/library/Iversia/FAQ/LikeHandler/Question.php
+++ upload/library/Iversia/FAQ/LikeHandler/Question.php
@@ -13,9 +13,9 @@ class Iversia_FAQ_LikeHandler_Question extends XenForo_LikeHandler_Abstract
 
 	public function getContentData(array $contentIds, array $viewingUser)
 	{
-		$qustionModel = XenForo_Model::create('Iversia_FAQ_Model_Question');
+		$questionModel = XenForo_Model::create('Iversia_FAQ_Model_Question');
 
-		return $questionModel->getById($contentIds);
+		return $questionModel->getQuestionsByIds($contentIds);
 	}
 
 	public function getListTemplateName()
